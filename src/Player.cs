015ab4a0cd0e8ff8ba0b7929a779351dca8923bc
Brad Parker <crustydog9@gmail.com>
2020-03-21 20:34:39 +1100

    Add files via upload

---
+++ src/Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Media;
+using System.Windows.Forms;
+
+namespace WavPlayer
+{
+    public partial class Player : Form
+    {
+        string fileName;
+
+        public Player()
+        {
+            InitializeComponent();
+        }
+
+        public void ChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFile.RestoreDirectory = true;
+            OpenFile.FileName = "";
+            OpenFile.Title = "Open .wav file";
+            OpenFile.Filter = "wav files (*.wav)|*.wav";
+            OpenFile.ShowDialog();
+
+            fileName = OpenFile.FileName;
+            FilePath.Text = fileName;
+        }
+
+        public void PlayButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SoundPlayer sound = new SoundPlayer(fileName);
+                sound.Play();
+            }
+            catch (InvalidOperationException)
+            {
+                MessageBox.Show("Invalid path or corrupted file");
+                return;
+            }
+            catch (ArgumentException)
+            {
+
+            }
+            
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SoundPlayer sound = new SoundPlayer(fileName);
+                sound.Stop();
+            }
+            catch (ArgumentException)
+            {
+                
+            }
+        }
+    }
+}
